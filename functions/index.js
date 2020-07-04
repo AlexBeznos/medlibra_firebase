255fb8491151e936e173a7da03406c47d72f0353
Alex Beznos <beznosa@yahoo.com>
2020-07-05 01:48:52 +0300

    Add function for subscriptions

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,29 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const Joi = require('joi');
+
+admin.initializeApp();
+
+exports.subscriptions = functions.region('europe-west1').https.onRequest(function(request, response) {
+  // STEP: Validation
+  const schema = Joi.object().keys({
+    email: Joi.string().email({ minDomainAtoms: 2 }).required()
+  });
+  
+  const { error, value } = Joi.validate(
+    request.body, 
+    schema, 
+    { stripUnknown: true }
+  );
+
+  if (error) {
+    return response.status(422).send({message: error.details[0].message});
+  }
+
+  // STEP: Record creation
+  const database = admin.firestore();
+  return database.collection('subscriptions')
+    .add(value)
+    .then(() => response.status(200).end());
+});
 
--- landing/__sapper__/build/client/client.661c0840.js
+++
@@ -1,1 +0,0 @@
-import"./client.11a3ce9b.js";
