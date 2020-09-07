8213bd084fa60b776ee34e078fa90df254717f24
Alex Beznos <beznosa@yahoo.com>
2020-09-07 11:00:46 +0300

    Add cors for functions and small improvements in texting

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,34 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const Joi = require('joi');
+const cors = require('cors')({
+  origin: true,
+});
 
 admin.initializeApp();
 
 exports.subscriptions = functions.region('europe-west1').https.onRequest(function(request, response) {
-  // STEP: Validation
-  const schema = Joi.object().keys({
-    email: Joi.string().email({ minDomainAtoms: 2 }).required()
+  cors(request, response, () => {
+    // STEP: Validation
+    const schema = Joi.object().keys({
+      email: Joi.string().email({ minDomainAtoms: 2 }).required()
+    });
+    
+    const { error, value } = Joi.validate(
+      request.body, 
+      schema, 
+      { stripUnknown: true }
+    );
+
+    if (error) {
+      return response.status(422).send({message: error.details[0].message});
+    }
+
+    // STEP: Record creation
+    const database = admin.firestore();
+    return database.collection('subscriptions')
+      .add(value)
+      .then(() => response.status(200).end());
   });
-  
-  const { error, value } = Joi.validate(
-    request.body, 
-    schema, 
-    { stripUnknown: true }
-  );
-
-  if (error) {
-    return response.status(422).send({message: error.details[0].message});
-  }
-
-  // STEP: Record creation
-  const database = admin.firestore();
-  return database.collection('subscriptions')
-    .add(value)
-    .then(() => response.status(200).end());
 });
 
